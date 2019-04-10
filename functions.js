ac867d3afda6e0ddc924746379d454c3aeb3ed53
inz <ionutinz@gmail.com>
2019-04-11 02:43:36 +0300

    Initial Commit

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+var contextMenuItem = {
+    "id": "inzDEX",
+    "title": "Caută definiția cuvântului în dicționar.",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "inzDEX" && clickData.selectionText){
+        var dexUrl = "https://dexonline.ro/definitie/";
+        var word = clickData.selectionText;
+        chrome.tabs.create({ url: dexUrl+word});
+    }
+})
